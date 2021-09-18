12276655a6ecabde3d3d2002f0e0bf0a6e6ec279
Victor <victor_brandao@outlook.com>
2021-09-18 17:45:42 -0300

    Backend initialized

---
+++ backend/src/main/java/com/logap/potencycurve/PotencyCurveApplication.java
@@ -0,0 +1,13 @@
+package com.logap.potencycurve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotencyCurveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotencyCurveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/logap/potencycurve/PotencyCurveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logap.potencycurve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotencyCurveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
