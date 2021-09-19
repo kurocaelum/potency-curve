c23d1d32d36aaf6650125ee1d9aa2fb3af39df22
Victor <victor_brandao@outlook.com>
2021-09-19 13:59:11 -0300

    Controllers

---
+++ backend/src/main/java/com/logap/potencycurve/controllers/CurveController.java
@@ -0,0 +1,26 @@
+package com.logap.potencycurve.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.logap.potencycurve.entities.Curve;
+import com.logap.potencycurve.services.CurveService;
+
+@RestController
+@RequestMapping(value = "/curve")
+public class CurveController {
+
+	@Autowired
+	private CurveService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Curve>> findAll() {
+		List<Curve> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+}
